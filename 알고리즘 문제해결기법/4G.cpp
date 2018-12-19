6aa9574779f49acdfb874b63ac09143a0785e305
yoon_ny <30654095+yoonnyeong@users.noreply.github.com>
2018-12-20 00:43:36 +0900

    Update 4G.cpp

--- 알고리즘 문제해결기법/4G.cpp
+++ 알고리즘 문제해결기법/4G.cpp
@@ -21,22 +21,18 @@ int main(){
 		if(op==0){  //출력
 			 int output = data[(p+k)%n];
 			 printf("%d\n", output);
-		
 		}
 		else if(op==1){// 왼쪽 이동
 			k = k % n; //이 부분을 꼭 넣어줘야 함
 		 	p = (p + k) % n; //p는 n을 넘어가면 안되니까
-		
 		}
-		else if(op==2){ // 오른쪽 이동 int k;
-				 	
+		else if(op==2){ // 오른쪽 이동 int k;	 	
 			k = k % n;
-				p = (p - k + n) % n;	//음수가 나올 경우를 고려하여 +
+			p = (p - k + n) % n;	//음수가 나올 경우를 고려하여 +
 		}
 		else if(op==3){
 				p = 0; //초기 배열
 		}
-	
 	}
 	return 0;
 }
