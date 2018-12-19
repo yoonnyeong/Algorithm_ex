8c3da4b5c5ce4a296262264d3f7bafefcba9c3ec
yoon_ny <30654095+yoonnyeong@users.noreply.github.com>
2018-12-19 14:32:43 +0900

    Create 4C.cpp

---
+++ 알고리즘 문제해결기법/4C.cpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+
+using namespace std;
+
+
+//최대공약수 -> 유클리드 호제법
+long long getGCD(long long a, long long b) {
+	while( a % b !=0){
+			long long c = a % b;
+			a = b;
+			b = c;
+	}
+	return b;
+}
+
+//최대공배수
+long long getLCM(long long a, long long b) {
+	// 두 수의 곱에서 최대 공약수를 나누면 최소 공배수다
+	return a * b / getGCD(a, b);
+}
+
+void process(int caseIndex) {
+	int num1, num2;
+	scanf("%d%d", &num1, &num2);
+
+	long long gcd =  getGCD(num1, num2);
+	long long lcm =  getLCM(num1, num2);
+
+	printf("Case #%d:\n", caseIndex);
+	printf("%lld %lld\n", gcd, lcm);
+}
+
+int main() {
+	int caseSize;
+	scanf("%d", &caseSize);
+
+	for (int caseIndex = 1; caseIndex <= caseSize; ++caseIndex) {
+		process(caseIndex);
+	}
+}
